ed20e7055411a39a554fa2a5a0fe9a9d907b81c3
darms <darmcca@gmail.com>
2017-04-11 14:34:01 -0700

    updated server file

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(`${__dirname}/build`));
 
-app.listen(PORT, function() {
-    console.log('server up:', PORT);
+app.listen(PORT, function(){
+ console.log('server up:', PORT);
 });
